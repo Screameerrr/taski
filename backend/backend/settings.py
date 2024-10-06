b1c84b13c0093873dd0f4db4aac552a73cd377ae
Maksim <maksik290896@yandex.ru>
2024-10-06 19:49:55 +0500

    /

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['89.169.162.39', '127.0.0.1', 'localhost', 'drochhub.zapto.org']
+ALLOWED_HOSTS = ['89.169.170.187', '127.0.0.1', 'localhost', 'drochhub.zapto.org']
 
 
 # Application definition
